598640e769a24e34a37342a7f736c7d5b5efa47b
Bobby Powers <bobbypowers@gmail.com>
2012-09-27 17:40:27 -0700

    add EnterChroot

---
+++ chroot.go
@@ -0,0 +1,18 @@
+package caps
+
+import (
+	"syscall"
+)
+
+// EnterChroot changes the current directory to the path specified,
+// and then calls chroot(2) with the same path.  This must be called
+// while the process has CAP_SYS_CHROOT.
+func EnterChroot(path string) (err error) {
+	if err = syscall.Chdir(path); err != nil {
+		return
+	}
+	if err = syscall.Chroot(path); err != nil {
+		return
+	}
+	return
+}
